a07a55920666458b1193ef91051513c2760e448b
kuzmrom7 <kuzmrom7@gmail.com>
2021-09-14 10:59:47 +0300

    fix export

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,7 +1,7 @@
-export { Card } from './Card';
-export { Button } from './Button';
-export { Calc } from './Calc';
-export { AppContent } from './Content';
-export { Header } from './Header';
-export { Total } from './Total';
-export { Wrap } from './Wrap';
+export * from './Card';
+export * from './Button';
+export * from './Calc';
+export * from './Content';
+export * from './Header';
+export * from './Total';
+export * from './Wrap';
